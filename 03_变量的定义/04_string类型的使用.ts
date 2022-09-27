2f8cfd6e1a28cdb6473893a00d36cad9f8ca669d
iamzjt-front-end <iamzjt@foxmail.com>
2022-09-27 17:30:01 +0800

    :white_check_mark: feat: 04_string类型的使用

---
+++ 03_变量的定义/04_string类型的使用.ts
@@ -0,0 +1,14 @@
+let message1: string = 'Hello World';
+let message2: string = 'Hello World';
+
+// 个人习惯: 默认情况下，如果可以推导出对应的标识符的类型时，一般情况下不加
+const name = 'IamZJT';
+const age = 18;
+const height = 1.88;
+
+let message3 = `name:${ name } age:${ age } height:${ height }`;
+console.log(message3);
+
+let result: boolean;
+
+export {}
