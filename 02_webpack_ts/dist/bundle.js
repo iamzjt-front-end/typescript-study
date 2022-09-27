64d926279c6aae8043f1587d7cb70c2910c59c11
iamzjt-front-end <iamzjt@foxmail.com>
2022-09-27 16:15:25 +0800

    :fire: feat: build

---
+++ 02_webpack_ts/dist/bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var o={564:(o,e)=>{Object.defineProperty(e,"__esModule",{value:!0}),e.sum=void 0,e.sum=function(o,e){return o+e}}},e={};function r(t){var s=e[t];if(void 0!==s)return s.exports;var n=e[t]={exports:{}};return o[t](n,n.exports,r),n.exports}(()=>{const o=r(564);console.log((0,o.sum)(20,30)),console.log("Hello World")})()})();
