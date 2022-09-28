151c14f8f2a73885340e22f51c6cea962d97f577
iamzjt-front-end <iamzjt@foxmail.com>
2022-09-28 13:17:32 +0800

    :white_check_mark: feat: 04_never类型的使用

--- 04_TypeScript的数据类型/04_never类型的使用.ts
+++ 04_TypeScript的数据类型/04_never类型的使用.ts
@@ -11,6 +11,7 @@ function bar(): never {
 
 
 function handleMessage(message : string | number) {
+// function handleMessage(message : string | number | boolean) {
   switch (typeof message) {
     case 'string':
       console.log('字符串');
@@ -19,6 +20,8 @@ function handleMessage(message : string | number) {
       console.log('数字');
       break;
     default:
+      // 形参不加类型，永远不可能到此，所以编译不会报错。
+      // 如果加了类型，但是煤堆此类型进行处理，编译过程中就会报错。
       let msg: never = message;
   }
 }
