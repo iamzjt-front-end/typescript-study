1815bc5c319e3146f80480b15ba72f736d92bdd2
iamzjt-front-end <iamzjt@foxmail.com>
2022-09-28 12:10:03 +0800

    :white_check_mark: feat: 01_any类型的使用

--- 04_TypeScript的数据类型/01_any类型的使用.ts
+++ 04_TypeScript的数据类型/01_any类型的使用.ts
@@ -1,3 +1,5 @@
+// 当进行一些类型断言 as any
+// 在不想给某些JavaScript变量添加具体的数据类型时(跟原生的JavaScript代码是一样的)
 let message: any = "abc";
 
 message = 123;
@@ -5,5 +7,13 @@ message = true;
 message = {
 
 }
+
+function foo(payload: any) {
+  console.log(payload);
+}
+
+foo(123);
+foo("abc");
+foo(null);
 
 console.log(message);
