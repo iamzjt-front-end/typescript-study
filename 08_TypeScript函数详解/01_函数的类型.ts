cac49ae1e6db31a081f0f0a2cb074aa9f93355ab
iamzjt-front-end <iamzjt@foxmail.com>
2022-10-08 18:17:36 +0800

    :green_heart: feat: 01_函数的类型

---
+++ 08_TypeScript函数详解/01_函数的类型.ts
@@ -0,0 +1,20 @@
+function foo() {
+
+}
+
+type FooFnType = () => void;
+
+function bar(fn: FooFnType) {
+  fn();
+}
+
+bar(foo);
+
+// 定义常量时，函数的类型
+type AddFnType = (num1: number, num2: number) => void;
+
+const add: AddFnType = (num1: number, num2: number) => {
+  return num1 + num2;
+}
+
+add(1, 2);
