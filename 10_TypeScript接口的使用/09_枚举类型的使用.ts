952b94a18020c29869b7fad15c4dc2e4201bebe9
iamzjt-front-end <iamzjt@foxmail.com>
2022-10-10 15:08:58 +0800

    :green_heart: feat: 09_枚举类型的使用

--- 10_TypeScript接口的使用/09_枚举类型的使用.ts
+++ 10_TypeScript接口的使用/09_枚举类型的使用.ts
@@ -6,7 +6,22 @@ enum Direction {
 }
 
 function turnDirection(direction: Direction) {
-  console.log(direction);
+  switch (direction) {
+    case Direction.LEFT:
+      console.log('左');
+      break;
+    case Direction.RIGHT:
+      console.log('右');
+      break;
+    case Direction.TOP:
+      console.log('上');
+      break;
+    case Direction.BOTTOM:
+      console.log('下');
+      break;
+    default:
+      break;
+  }
 }
 
 turnDirection(Direction.LEFT);
