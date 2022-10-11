da2bc45b529a12077107223cfd864a0564dc1665
iamzjt-front-end <iamzjt@foxmail.com>
2022-10-11 14:39:52 +0800

    :memo: feat: namespace

---
+++ 12_TypeScript其他内容补充/src/utils/format.ts
@@ -0,0 +1,11 @@
+export namespace Time {
+  export function format(time: string) {
+    return '2022-02-22';
+  }
+}
+
+export namespace Price {
+  export function format(time: string) {
+    return '222.22';
+  }
+}
