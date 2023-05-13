84c66b502b2dbd453d7f1cb2fc1243cedcd3dec5
iamzjt-front-end <iamzjt@foxmail.com>
2023-05-13 21:16:05 +0800

    ⚡️ feat: 02_常用的24种TS类型.ts

--- mkw/01_深入TS核心语法+各种实战应用/02_常用的24种TS类型.ts
+++ mkw/01_深入TS核心语法+各种实战应用/02_常用的24种TS类型.ts
@@ -1,1 +1,19 @@
-// * 基本类型
+// * 1. 基本类型
+// - number, string, boolean, symbol, null, undefined
+
+// * 2. 根类型
+// - Object, {}
+
+// * 3. 对象类型
+// - Array, object, function
+
+// * 4. 枚举
+// - enum
+
+// * 5. 其他特殊类型
+// - any, unknown, never, void, tuple, 可变元组
+
+// * 6. 合成类型
+// - 联合类型, 交叉类型
+
+// * 7. 字面量数据类型
